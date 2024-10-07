308465d81f1a92a7b72e62a1874f05c1627e8387
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2024-10-07 20:50:26 -0300

    Adicionando mensagem de sucesso ao cadastrar projeto e preparação para mostrar projetos cadastrados na tela de Projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,27 @@
+import Message from '../layout/Message'
+import { useLocation } from 'react-router-dom'
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
 function Projects() {
-    return <h1>Projects</h1>
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            {message && <Message message={message} type='success'/>}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+        </div>
+    )
 }
 
 export default Projects
