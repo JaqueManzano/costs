2be7124f1a6a160fb66dd9d6e29f4842c9f29f70
cst_jaqueline.santos <cst_jaqueline.santos@minervafoods.com>
2024-10-08 21:29:58 -0300

    Implementação dos cards da tela de Projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,13 +3,32 @@ import { useLocation } from 'react-router-dom'
 import styles from './Projects.module.css'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+import { useState, useEffect } from 'react'
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/projects',{
+            method: 'GET',
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((error) => console.log(error))
+    }, [])
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -18,7 +37,17 @@ function Projects() {
             </div>
             {message && <Message message={message} type='success'/>}
             <Container customClass="start">
-                <p>Projetos...</p>
+            {projects.length > 0 &&
+             projects.map((project) => (
+                <ProjectCard 
+                id={project.id}
+                name={project.name}
+                budget={project.budget}
+                category={project.category?.name}
+                key={project.id}
+                />
+             ))
+            }
             </Container>
         </div>
     )
